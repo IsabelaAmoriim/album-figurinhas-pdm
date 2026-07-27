618478fb08b98da00c9f669d59a2382eedd381ac
Heitor Borges Resende <85977451+HeitorBrgs@users.noreply.github.com>
2026-07-26 22:33:52 -0300

    Delete album-figurinhas-pdm-main directory

--- album-figurinhas-pdm-main/app/src/main/java/com/album/figurinha/model/ColecaoFigurinha.kt
+++
@@ -1,9 +0,0 @@
-package com.album.figurinha.model
-
-data class ColecaoFigurinha(
-    val id: Int,
-    val stickerId: Int,
-    val quantity: Int,
-    val unlocked: Boolean,
-    val repeated: Boolean
-)
--- album-figurinhas-pdm-main/app/src/main/java/com/album/figurinha/model/PacoteFigurinha.kt
+++
@@ -1,9 +0,0 @@
-package com.album.figurinha.model
-
-import java.time.LocalDateTime
-
-data class PacoteFigurinha(
-    val id: Int,
-    val openDate: LocalDateTime,
-    val stickers: List<Figurinha> = emptyList()
-)
